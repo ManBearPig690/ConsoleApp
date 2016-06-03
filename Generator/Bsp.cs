981717939d980c0ce8f7a789071f16d359df39a8
ManBearPig690 <jamesfleuret@live.com>
2016-06-03 11:28:54 -0700

    Added more room generation

--- Generator/Bsp.cs
+++ Generator/Bsp.cs
@@ -31,6 +31,72 @@ namespace Generator
             _roomList = new List<BspRoom>();
             var rootRoom = new BspRoom(0, 0, PcGridWidth, PcGridHeight, _roomSize);
             _roomList.Add(rootRoom);
+
+            while (_roomList.Count - 1 < _roomNum)
+            {
+                var splitIndex = new Random().Next(_roomList.Count - 1);
+                var splitRoom = _roomList[splitIndex];
+                if (splitRoom.SplitBsp())
+                {
+                    _roomList.Add(splitRoom.LeftChild);
+                    _roomList.Add(splitRoom.RightChiled);
+                }
+            }
+
+            // go through each leaf and create room
+            for (var i = 0; i < _roomList.Count - 1; i++)
+            {
+                var rm = _roomList[i];
+
+                if(rm.LeftChild != null || rm.RightChiled != null) continue; // if has child leaf go to next
+                
+                rm.GenerateBspRoom();
+
+                for (var j = rm.X; j < rm.X + rm.Width; j++)
+                {
+                    PcGrid[j][rm.Y] = 5;
+                    PcGrid[j][rm.Y + rm.Height - 1] = 5;
+                }
+                for (var k = rm.Y; k < rm.Y + rm.Height; k++)
+                {
+                    PcGrid[rm.X][k] = 5;
+                    PcGrid[rm.X + rm.Width - 1][k] = 5;
+                }
+            }
+            
+            // go through each leaf and create room
+            for (var i = 0; i < _roomList.Count - 1; i++)
+            {
+                var rm = _roomList[i];
+
+                if (rm.LeftChild != null || rm.RightChiled != null) continue; // if has child leaf go to next
+
+                // Horizontal walls
+                // fills grid with values represent where walls should be drawn
+                for (var j = rm.RoomX1; j <= rm.RoomX2; j++)
+                {
+                    PcGrid[j][rm.RoomY1] = 2; // north wall
+                    PcGrid[j][rm.RoomY2] = 2; // south wall
+                }
+
+                // vertical walls
+                for (var k = rm.RoomY1; k <= rm.RoomY2; k++)
+                {
+                    PcGrid[rm.RoomX1][k] = 2; // West Wall
+                    PcGrid[rm.RoomX2][k] = 2; // East Wall
+                }
+
+                // Create the Room
+                for(var j = rm.RoomY1 + 1; j < rm.RoomY2; j++)
+                    for (var k = rm.RoomX1 + 1; i < rm.RoomX2; i++)
+                        PcGrid[k][j] = 1; // fill the room => 1 = traversable
+            }
+            ConnectBspRoom(rootRoom);
+        }
+
+        private void ConnectBspRoom(BspRoom room)
+        {
+            
         }
     }
 }
--- Generator/BspRoom.cs
+++ Generator/BspRoom.cs
@@ -10,41 +10,78 @@ namespace Generator
     public class BspRoom
     {
         private int _minSize;
-        private int _x;
-        private int _y;
-        private int _width;
-        private int _height;
-        private int _roomX1;
-        private int _roomY1;
-        private int _roomX2;
-        private int _roomY2;
         private bool _isFilled = false;
         private bool _horizontal;
-        private BspRoom _leftChild;
-        private BspRoom _rightChiled;
+
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+        public int RoomX1;
+        public int RoomY1;
+        public int RoomX2;
+        public int RoomY2;
+        public BspRoom LeftChild;
+        public BspRoom RightChiled;
 
         public BspRoom(int x, int y, int w, int h, int size)
         {
-            _x = x;
-            _y = y;
-            _width = w;
-            _height = h;
+            X = x;
+            Y = y;
+            Width = w;
+            Height = h;
             _minSize = size;
         }
 
+        /// <summary>
+        /// splits the grid into chuncks
+        /// </summary>
+        /// <returns></returns>
         public bool SplitBsp()
         {
-            var r = new Random((int)DateTime.Now.Ticks);
-            if(_leftChild != null) return false; // already split
+            var random = new Random((int)DateTime.Now.Ticks); // helps to crate more psudo random numbers
+
+            var r = new Random((int) DateTime.Now.Ticks);
+            if (LeftChild != null) return false; // already split
 
             _horizontal = r.Next(100) > 50;
 
-            var maxSize = (_horizontal ? _height : _width) - _minSize;
+            var maxSize = (_horizontal ? Height : Width) - _minSize; // max height/widht that can be split off
             if (maxSize <= _minSize) return false; // area to small
 
-            //var split
+            var splitPoint = random.Next(maxSize); // generate random split point
+            if (splitPoint < _minSize) splitPoint = _minSize; // adjust point so they are both at least the min size
 
-            return true;
+            if (_horizontal)
+            {
+                LeftChild = new BspRoom(X, Y, Width, splitPoint, _minSize);
+                RightChiled = new BspRoom(X, Y + splitPoint, Width, Height - splitPoint, _minSize);
+            }
+            else
+            {
+                LeftChild = new BspRoom(X, Y, splitPoint, Height, _minSize);
+                RightChiled = new BspRoom(X + splitPoint, Y, Width - splitPoint, Height, _minSize);
+            }
+
+            return true; // successfull split
+        }
+
+        // Generates a room inside the area
+        public void GenerateBspRoom()
+        {
+            if (LeftChild != null) return;
+
+            var random = new Random((int) DateTime.Now.Ticks);
+            var minX = ((Width * 3 / 4) > _minSize) ? (Width * 3 / 4) : _minSize;
+            var minY = ((Height * 3 / 4) > _minSize) ? (Height * 3 / 4) : _minSize;
+            RoomX1 = X + ((Width - _minSize) <= 0 ? 0 : random.Next((Width - minX)));
+            RoomY1 = Y + ((Height - _minSize) <= 0 ? 0 : random.Next((Height - minY)));
+
+            var randomWidth = random.Next(Width - RoomX1);
+            var randomHeight = random.Next(Height - RoomX1);
+            RoomX2 = RoomX1 + (randomWidth > minX ? randomWidth : minX) - 1;
+            RoomY2 = RoomY1 + (randomHeight > minY ? randomHeight : minY) - 1;
+            _isFilled = true;
         }
 
     }
